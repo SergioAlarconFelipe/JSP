af54659dfb9a218b48f4879e006a03445e2f5bfb
Sergio Alarcón Felipe <sergio.alarcon.felipe@gmail.com>
2017-01-24 23:00:28 +0100

    Update getAllParametersServlet.java

--- getAllParametersServlet.java
+++ getAllParametersServlet.java
@@ -1,22 +1,36 @@
-    public void handleRequest(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        PrintWriter out = res.getWriter();
-        res.setContentType("text/plain");
+public ArrayList getAllParametersServlet (HttpServletRequest req) {
+    ArrayList <String> array = new ArrayList();
 
-        Enumeration<String> parameterNames = req.getParameterNames();
+    Enumeration<String> parameterNames = req.getParameterNames();
 
-        while (parameterNames.hasMoreElements()) {
-            String paramName = parameterNames.nextElement();
-            out.write(paramName);
-            out.write(" ");
+    while (parameterNames.hasMoreElements()) {
+        String paramName = parameterNames.nextElement();
+        array.add(paramName);
 
-            String[] paramValues = req.getParameterValues(paramName);
-            for (int i = 0; i < paramValues.length; i++) {
-                    String paramValue = paramValues[i];
-                    out.write(" " + paramValue);
-                    out.write(" ");
-            }
+    }
 
+    return array;
+}    
+
+public void handleRequest(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    PrintWriter out = res.getWriter();
+    res.setContentType("text/plain");
+
+    Enumeration<String> parameterNames = req.getParameterNames();
+
+    while (parameterNames.hasMoreElements()) {
+        String paramName = parameterNames.nextElement();
+        out.write(paramName);
+        out.write(" ");
+
+        String[] paramValues = req.getParameterValues(paramName);
+        for (int i = 0; i < paramValues.length; i++) {
+                String paramValue = paramValues[i];
+                out.write(" " + paramValue);
+                out.write(" ");
         }
 
-        out.close();
     }
+
+    out.close();
+}
